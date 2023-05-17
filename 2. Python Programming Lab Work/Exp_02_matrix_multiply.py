5a7e778d3b33e7d9a8741e4c719460293df5a000
Devanshu <devanshukd007@gmail.com>
2023-05-17 20:34:24 +0530

    Python : Practical 02

--- 2. Python Programming Lab Work/Exp_02_matrix_multiply.py
+++ 2. Python Programming Lab Work/Exp_02_matrix_multiply.py
@@ -1,5 +1,6 @@
-# take a normal example
+# Python Practical 02: To write a python a program to perform Matrix Multiplication.
 
+# take a normal example
 x = [[5, 7, 8], [4, 12, 3], [7, 23, 9]]
 y = [[5, 2, 8], [6, 5, 1], [12, 4, 2]]
 
