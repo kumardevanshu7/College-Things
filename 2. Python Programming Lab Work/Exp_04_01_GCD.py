e17ccf35e318bc3c2500adc1a8e0bb53301e1c41
Devanshu <devanshukd007@gmail.com>
2023-05-17 19:54:44 +0530

    higher error solved

---
+++ 2. Python Programming Lab Work/Exp_04_01_GCD.py
@@ -0,0 +1,13 @@
+# Experiment No. 4 - Part A - Compute the GCD of Two Numbers using Math Function
+
+# import the math function
+import math
+
+# taking inputs from user
+num_01 = int(input("Enter First Number: "))
+num_02 = int(input("Enter Second Number: "))
+
+# using math.gcd funtion from math library
+GCD = math.gcd(num_01, num_02)
+
+print("The GCD of", num_01, "and", num_02, "is", GCD, ".")
---
+++ 2. Python Programming Lab Work/Exp_04_03_GCD.py
@@ -0,0 +1,12 @@
+# Experiment No. 4 - Part C - Find exponential of a number
+
+# import the math function
+import math
+
+# taking input from user
+num = int(input("Enter Number: "))
+
+# using math.exp funtion from math library
+result = math.exp(num)
+
+print(result)
