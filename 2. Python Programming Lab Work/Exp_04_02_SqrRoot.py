97747e600cf7a21c7ba0d405f2f70138e20df0d0
Devanshu <devanshukd007@gmail.com>
2023-05-17 20:40:03 +0530

    Python : Practical 04 - Part 2

--- 2. Python Programming Lab Work/Exp_04_02_SqrRoot.py
+++ 2. Python Programming Lab Work/Exp_04_02_SqrRoot.py
@@ -1,4 +1,4 @@
-# Python Practical 04 - Part B - Find the square root of a number using math function
+# Python Practical 04 - Part B - Find the square root of a number using math function.
 
 # import the math function
 import math
@@ -10,4 +10,8 @@ num = int(input("Enter Number: "))
 ans = math.sqrt(num)
 
 print("The Square Root of", num, "is", ans, ".")
+
+print("\n\n\n Name: Kumar Devanshu \t Roll No: 2200970139009 \t Section: IT-A2")
+
+print("\n")
 
