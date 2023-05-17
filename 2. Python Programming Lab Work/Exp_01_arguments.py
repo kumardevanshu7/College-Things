1744fadd549052f4793b73ea0c2439d7f43e4d95
Devanshu <devanshukd007@gmail.com>
2023-05-17 20:11:08 +0530

    Python Practical 01

---
+++ 2. Python Programming Lab Work/Exp_01_arguments.py
@@ -0,0 +1,11 @@
+import sys
+
+#command-line argumnets
+arguments = sys.argv[1:]
+
+# now printing the arguments
+print(f"Arguments are: {arguments}")
+
+print("\n\n\n\n Name: Kumar Devanshu \t Roll No: 2200970139009 \t Section: IT-A2")
+
+print("\n\n")
