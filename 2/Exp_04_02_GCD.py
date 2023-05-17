d94438d9a8f338b779d991b64a955b0729924395
Devanshu <devanshukd007@gmail.com>
2023-05-17 19:49:07 +0530

    Changes

---
+++ 2/Exp_04_02_GCD.py
@@ -0,0 +1,13 @@
+# Experiment No. 4 - Part B - Find the square root of a number using math function
+
+# import the math function
+import math
+
+# taking input from user
+num = int(input("Enter Number: "))
+
+# using math.sqrt funtion from math library
+ans = math.sqrt(num)
+
+print("The Square Root of", num, "is", ans, ".")
+
