a8a07bad1a5c7c8a2848d0ff715452b7e490defb
Devanshu <devanshukd007@gmail.com>
2023-06-20 21:59:29 +0530

    Python : Practical 13 : Linear Search

--- 2. Python Programming Lab Work/Exp_13_Linear_Search.py
+++ 2. Python Programming Lab Work/Exp_13_Linear_Search.py
@@ -1,17 +1,19 @@
-def linear_search(arr, target):
+# Python Practical 13 : To write a python program to perform Linear Search.
+
+def linear_search(arr, item):
     for i in range(len(arr)):
-        if arr[i] == target:
+        if arr[i] == item:
             return i  
     return -1  
 
-numbers = [5, 9, 2, 11, 3, 8, 6]
-target_value = 3
-index = linear_search(numbers, target_value)
+nums = [5, 9, 2, 11, 3, 8, 6]
+item = 3
+idx = linear_search(nums, item)
 
-if index != -1:
-    print("The target value", target_value, "is found at index", index)
+if idx != -1:
+    print("The item", item, "is found at index.", idx)
 else:
-    print("The target value", target_value, "is not found in the list.")
+    print("The item", item, "is not found in the list.")
 
 print("\n\n\n Name: Kumar Devanshu \t Roll No: 2200970139009 \t Section: IT-A2")
 
