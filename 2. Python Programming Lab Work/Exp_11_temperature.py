c6d4f3e98fd3aa39d51ff5d5405203ad196b4e98
Devanshu <devanshukd007@gmail.com>
2023-06-20 23:15:32 +0530

    Python : Practical 11 : Convert Temperature

--- 2. Python Programming Lab Work/Exp_11_temperature.py
+++ 2. Python Programming Lab Work/Exp_11_temperature.py
@@ -1,3 +1,5 @@
+# Python Practical 11 : To write a python program that asks the user for a temperature in celcius and converts and outputs the temperature in Fehrenheit.
+
 celsius = float(input("Enter a temperature in Celsius: "))
 
 fahrenheit = (celsius * 9/5) + 32
